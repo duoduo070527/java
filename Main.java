53f219a5138adb6934f7e4c92e07dcda9ab031dc
duoduo070527 <20119411@setu.ie>
2026-07-27 14:42:52 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,37 @@
 
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
-// 1-100猜数字小游戏，最多5次机会
+// 猜周杰伦歌名小游戏，一共有5次猜测机会
 public class Main {
     public static void main(String[] args) {
-        Random r = new Random();
         Scanner sc = new Scanner(System.in);
-        // 生成1~100随机数字
-        int target = r.nextInt(100) + 1;
+        Random r = new Random();
+
+        // 周杰伦经典歌名单
+        String[] songList = {"七里香", "青花瓷", "稻香", "晴天", "搁浅", "夜曲", "枫", "退后"};
+        // 随机选中一首歌
+        String targetSong = songList[r.nextInt(songList.length)];
         int count = 0; // 记录猜测次数
 
-        System.out.println("====猜数字游戏开始====");
-        System.out.println("系统生成了1-100之间的数字，你一共有5次机会！");
+        System.out.println("====猜周杰伦歌名游戏开始====");
+        System.out.println("系统随机挑选了一首周杰伦歌曲，你一共有5次机会！");
 
         while (true) {
             count++;
-            // 超过5次直接结束游戏
             if (count > 5) {
-                System.out.println("5次机会用完，游戏失败！正确数字是：" + target);
+                System.out.println("5次机会用完，游戏失败！正确歌名是：" + targetSong);
                 break;
             }
-            System.out.print("请输入你猜的数字：");
-            int num = sc.nextInt();
 
-            if (num > target) {
-                System.out.println("猜大了！还剩" + (5 - count) + "次机会");
-            } else if (num < target) {
-                System.out.println("猜小了！还剩" + (5 - count) + "次机会");
-            } else {
+            System.out.print("请输入你猜测的歌名：");
+            String inputName = sc.next();
+
+            if (inputName.equals(targetSong)) {
                 System.out.println("恭喜你猜对了！一共猜了" + count + "次");
                 break;
+            } else {
+                System.out.println("猜错啦！还剩" + (5 - count) + "次机会");
             }
         }
         sc.close();
